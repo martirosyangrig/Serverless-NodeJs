b86542993fcbd99d7e87333c81bff6bacd465a3b
martirosyangrig <grigor.martriosyan2000@mail.ru>
2024-05-09 15:50:39 +0400

    First lambda with webpack and serverless

---
+++ lambdas/getUser.js
@@ -0,0 +1,27 @@
+const Responses = require("./API_Responsed");
+
+module.exports.handler = async (event) => {
+    console.log("event", event);
+
+    if (!event.pathParameters || !event.pathParameters.ID) {
+        // failed withount an ID
+        return Responses._400({ message: "Missing the ID from the path" });
+    }
+
+    let ID = event.pathParameters.ID;
+
+    if (data[ID]) {
+        //return the data
+        return Responses._200(data[ID]);
+    }
+
+    //failed as ID not in the data
+    return Responses._400({ message: "No Id in data" });
+};
+
+const data = {
+    1234: { name: "Anna Jones", age: 25, job: "journalist" },
+    7893: { name: "Chris Smith", age: 25, job: "teacher" },
+    5132: { name: "Tom Hague", age: 25, job: "dumb" },
+    4544: { name: "Greg Marti", age: 25, job: "programmer" },
+};
