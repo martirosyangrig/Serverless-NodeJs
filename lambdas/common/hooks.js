d8a2f18e0bd0b0e87eaa5a4b0ffbe469e8561e9d
martirosyangrig <grigor.martriosyan2000@mail.ru>
2024-05-16 18:40:38 +0400

    Validation with cutom Hooks

--- lambdas/common/hooks.js
+++ lambdas/common/hooks.js
@@ -1,9 +1,74 @@
-const { useHooks, logEvent, parseEvent, handleUnexpectedError } = require("lambda-hooks")
+const {
+    useHooks,
+    logEvent,
+    parseEvent,
+    handleUnexpectedError,
+} = require("lambda-hooks");
 
 const withHooks = useHooks({
     before: [logEvent, parseEvent],
     after: [],
-    onError: [handleUnexpectedError]
+    onError: [handleUnexpectedError],
 });
 
-module.exports = { withHooks };
+const hooksWithValidation = ({ bodySchema, pathSchema }) => {
+    return useHooks(
+        {
+            before: [logEvent, parseEvent, validateEventBody, validatePath],
+            after: [],
+            onError: [handleUnexpectedError],
+        },
+        {
+            bodySchema,
+            pathSchema,
+        }
+    );
+};
+
+const validateEventBody = async (state) => {
+    const { bodySchema } = state.config;
+
+    if (!bodySchema) {
+        throw new Error("No body schema provided");
+    }
+
+    try {
+        const { event } = state;
+
+        await bodySchema.validate(event.body, { strict: true });
+    } catch (error) {
+        console.log(error, "Error in validateEventBody");
+        state.exit = true;
+        state.response = {
+            statusCode: 400,
+            body: JSON.stringify({ error: error.message }),
+        };
+    }
+
+    return state;
+};
+
+const validatePath = async (state) => {
+    const { pathSchema } = state.config;
+
+    if (!pathSchema) {
+        throw new Error("No body schema provided");
+    }
+
+    try {
+        const { event } = state;
+
+        await pathSchema.validate(event.pathParameters, { strict: true });
+    } catch (error) {
+        console.log(error, "Error in validatePath");
+        state.exit = true;
+        state.response = {
+            statusCode: 400,
+            body: JSON.stringify({ error: error.message }),
+        };
+    }
+
+    return state;
+};
+
+module.exports = { withHooks, hooksWithValidation };
--- lambdas/endpoints/createPlayerScore.js
+++ lambdas/endpoints/createPlayerScore.js
@@ -1,17 +1,20 @@
+const yup = require("yup");
 const Responses = require("../common/API_Responsed");
 const Dynamo = require("../common/Dynamo");
-const { withHooks } = require("../common/hooks");
+const { hooksWithValidation } = require("../common/hooks");
 
 const tableName = process.env.tableName;
 
+const bodySchema = yup.object().shape({
+    name: yup.string().required(),
+    score: yup.number().required(),
+});
+
+const pathSchema = yup.object().shape({
+    ID: yup.string().required(),
+});
 
 const handler = async (event) => {
-
-    if (!event.pathParameters.ID) {
-        // failed withount an ID
-        return Responses._400({ message: "Missing the ID from the path" });
-    }
-
     let ID = event.pathParameters.ID;
     const user = event.body;
     user.ID = ID;
@@ -19,10 +22,12 @@ const handler = async (event) => {
     const newUser = await Dynamo.write(user, tableName);
 
     if (!newUser) {
-        return Responses._400({message: "Failed to write user by ID"})
+        return Responses._400({ message: "Failed to write user by ID" });
     }
 
-    return Responses._200({newUser});
-}
+    return Responses._200({ newUser });
+};
 
-module.exports.handler = withHooks(handler);
+module.exports.handler = hooksWithValidation({ bodySchema, pathSchema })(
+    handler
+);
